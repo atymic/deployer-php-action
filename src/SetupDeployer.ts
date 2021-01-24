72576765a6fe4590e5123cd8ffa9c9df6a3b65f5
Yuriy Stovbur <script.kz@gmail.com>
2021-01-24 13:16:04 +0600

    install recipes only for deployer 6 and earlier

--- src/SetupDeployer.ts
+++ src/SetupDeployer.ts
@@ -3,7 +3,17 @@ const core = require('@actions/core');
 
 interface DeployerOptions {
     deployerVersion?: string,
+    deployerRecipesVersion?: string,
     skipDeployerInstall?: string,
+}
+
+const getInstalledDeployerMajorVersion = async (): Promise<number|null> => {
+    const {stdout} = await execa('composer', ['global', 'show', 'deployer/deployer']);
+
+    const regexp = /^versions.*?(\d+)/im;
+    const matches = regexp.exec(stdout);
+
+    return matches && +matches[1];
 }
 
 export default async (options: DeployerOptions): Promise<void> => {
@@ -14,6 +24,16 @@ export default async (options: DeployerOptions): Promise<void> => {
         : 'deployer/deployer';
 
     await execa('composer', ['global', 'require', deployerPackage]);
+
+    const deployerMajor = await getInstalledDeployerMajorVersion();
+
+    if (deployerMajor && deployerMajor <= 6) {
+        const deployerRecipesPackage = options.deployerRecipesVersion
+            ? `deployer/recipes:${options.deployerRecipesVersion}`
+            : 'deployer/recipes';
+
+        await execa('composer', ['global', 'require', deployerRecipesPackage]);
+    }
 
     const installPath = (await execa('composer', ['global', 'config', 'home'])).stdout;
     core.addPath(`${installPath}/vendor/bin`);
