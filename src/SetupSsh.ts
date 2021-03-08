5166a2ec11a1f1a6096439bd1fdad85bb989b45e
Michael Leister <michael@we-dev.at>
2021-03-08 18:15:49 +0100

    Make compatible with self-hosted
    * Do not open multiple ssh-agents
    * Do not append known_hosts multiple times

--- src/SetupSsh.ts
+++ src/SetupSsh.ts
@@ -1,32 +1,57 @@
 const execa = require('execa');
 const core = require('@actions/core');
 const promise = require('bluebird');
+const os = require('os');
 const fs = promise.promisifyAll(require('fs'));
 
 interface SshOptions {
-    knownHosts: string,
-    privateKey: string,
-    disableHostKeyChecking: boolean,
+  knownHosts: string,
+  privateKey: string,
+  disableHostKeyChecking: boolean,
+}
+
+const fileIncludesString = async (fileName: String, content: String): Promise<boolean> => {
+  return new Promise<boolean>((resolve, reject) => {
+    fs.readFile(fileName, function (err: any, fileData: any) {
+      if (err)
+        return resolve(false);
+      else if (fileData.includes(content))
+        return resolve(true);
+      else
+        return resolve(false);
+    });
+  });
 }
 
 export default async (options: SshOptions) => {
-    const home = process.env['HOME'];
-    const sshHome = home + '/.ssh';
-    fs.mkdirAsync(sshHome, {recursive: true});
+  const home = process.env['HOME'];
+  const sshHome = home + '/.ssh';
+  fs.mkdirAsync(sshHome, {recursive: true});
 
-    const authSock = '/tmp/ssh-auth.sock';
-    await execa('ssh-agent', ['-a', authSock]);
-    core.exportVariable('SSH_AUTH_SOCK', authSock);
+  const authSock = '/tmp/ssh-auth.sock';
+  core.exportVariable('SSH_AUTH_SOCK', authSock);
 
-    // Fix private key line endings
-    const privateKey = options.privateKey.replace('/\r/g', '').trim() + '\n';
-    await execa('ssh-add', ['-'], {input: privateKey});
+  if (fs.existsSync(authSock)) {
+    core.info('Skipping SSH agent setup as agent is already running.');
+    return;
+  }
 
-    if (options.disableHostKeyChecking) {
-        await fs.appendFileAsync(`/etc/ssh/ssh_config`, `StrictHostKeyChecking no`);
-        return;
-    }
+  await execa('ssh-agent', ['-a', authSock]);
 
-    await fs.appendFileAsync(`${sshHome}/known_hosts`, options.knownHosts);
-    await fs.chmodAsync(`${sshHome}/known_hosts`, '644');
+  // Fix private key line endings
+  const privateKey = options.privateKey.replace('/\r/g', '').trim() + '\n';
+  await execa('ssh-add', ['-'], {input: privateKey});
+
+  if (options.disableHostKeyChecking) {
+    await fs.appendFileAsync(`/etc/ssh/ssh_config`, `StrictHostKeyChecking no`);
+    return;
+  }
+
+  const knownHostsFileLocation = `${sshHome}/known_hosts`;
+  const isHostAlreadyAppended = await fileIncludesString(knownHostsFileLocation, options.knownHosts);
+
+  if (!isHostAlreadyAppended) {
+    await fs.appendFileAsync(knownHostsFileLocation, os.EOL + options.knownHosts);
+    await fs.chmodAsync(knownHostsFileLocation, '644');
+  }
 };
